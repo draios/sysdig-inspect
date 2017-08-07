f641bf229a7ef6f15758df2d8c15dbcc0663b373
Elia <frodohack@gmail.com>
2017-08-07 23:25:53 +0200

    Added dmg electron package for darwin

--- ember-electron/electron-forge-config.js
+++ ember-electron/electron-forge-config.js
@@ -1,27 +1,20 @@
 module.exports = {
-  "make_targets": {
-    "win32": [
-      "squirrel"
-    ],
-    "darwin": [
-      "zip"
-    ],
-    "linux": [
-      "deb",
-      "rpm"
-    ]
+  'make_targets': {
+    'win32': ['squirrel'],
+    'darwin': ['zip', 'dmg'],
+    'linux': ['deb', 'rpm']
   },
-  "electronPackagerConfig": {},
-  "electronWinstallerConfig": {
-    "name": ""
+  'electronPackagerConfig': {},
+  'electronWinstallerConfig': {
+    'name': ''
   },
-  "electronInstallerDebian": {},
-  "electronInstallerRedhat": {},
-  "github_repository": {
-    "owner": "",
-    "name": ""
+  'electronInstallerDebian': {},
+  'electronInstallerRedhat': {},
+  'github_repository': {
+    'owner': '',
+    'name': ''
   },
-  "windowsStoreConfig": {
-    "packageName": ""
+  'windowsStoreConfig': {
+    'packageName': ''
   }
 };
