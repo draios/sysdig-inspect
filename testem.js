f9984a6bdf26e5f6092dc5210f2a8e8673182add
Elia <frodohack@gmail.com>
2017-08-08 20:21:36 +0200

    Try Chrome for cli tests

--- testem.js
+++ testem.js
@@ -3,10 +3,11 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'PhantomJS'
+    // 'PhantomJS'
+    'Chrome'
   ],
   launch_in_dev: [
-    'PhantomJS',
+    // 'PhantomJS',
     'Chrome'
   ]
 };
