c6a4ff07f2dee06c9a592992cfef9824352fa12b
Elia <frodohack@gmail.com>
2017-08-16 10:15:36 +0200

    Configured electron package for all platform

--- ember-electron/electron-forge-config.js
+++ ember-electron/electron-forge-config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  'make_targets': {
-    'win32': ['squirrel'],
-    'darwin': ['zip', 'dmg'],
-    'linux': ['deb', 'rpm']
-  },
-  'electronPackagerConfig': {},
-  'electronWinstallerConfig': {
-    'name': ''
-  },
-  'electronInstallerDebian': {},
-  'electronInstallerRedhat': {},
-  'github_repository': {
-    'owner': '',
-    'name': ''
-  },
-  'windowsStoreConfig': {
-    'packageName': ''
-  }
+    'make_targets': {
+        'win32': ['squirrel'],
+        'darwin': ['zip', 'dmg'],
+        'linux': ['deb', 'rpm']
+    },
+    'electronPackagerConfig': {
+        'name': 'wsysdig',
+        'all': true,
+        'overwrite': true
+    },
+    'electronWinstallerConfig': {
+        'name': ''
+    },
+    'electronInstallerDebian': {},
+    'electronInstallerRedhat': {},
+    'github_repository': {
+        'owner': '',
+        'name': ''
+    }
 };
