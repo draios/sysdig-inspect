563c10a3902664928a06957c73eb28b7a44f5ea3
davideschiera <davide.schiera@gmail.com>
2017-08-16 00:29:08 -0700

    Time series

--- tests/integration/components/sd-time-series-test.js
+++ tests/integration/components/sd-time-series-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('time-series', 'Integration | Component | time series', {
+moduleForComponent('sd-time-series', 'Integration | Component | time series', {
     integration: true,
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
 
-    this.render(hbs`{{time-series}}`);
+    this.render(hbs`{{sd-time-series}}`);
 
     assert.equal(this.$().text().trim(), '');
 
     // Template block usage:
     this.render(hbs`
-        {{#time-series}}
+        {{#sd-time-series}}
             template block text
-        {{/time-series}}
+        {{/sd-time-series}}
     `);
 
     assert.equal(this.$().text().trim(), 'template block text');
