97453a612658d93daf76e7ece04f527d867a2100
Elia <frodohack@gmail.com>
2017-08-17 20:46:52 +0200

    Removed initial transition for electron

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
-import config from '../appConfig';
 
 export default Ember.Route.extend({
-    beforeModel() {
-        if (config.isElectron()) {
-            this.replaceWith('electron');
-        }
-    },
 });
