b46b72a959f2ceaf87d76c453c8298bd1ea4f989
Elia <frodohack@gmail.com>
2017-08-17 14:28:47 +0200

    Added font-awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,6 +2,8 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
     let app = new EmberApp(defaults, {
@@ -20,7 +22,17 @@ module.exports = function(defaults) {
     // modules that you would like to import into your application
     // please specify an object with the list of modules as keys
     // along with the exports of each module as its value.
+
     app.import(app.bowerDirectory + '/normalize-css/normalize.css');
 
-    return app.toTree();
+    var fontawesome = new funnel('bower_components/font-awesome/fonts', {
+        srcDir: '/',
+        destDir: 'fonts'
+    });
+
+    var merged = mergeTrees([app.toTree(), fontawesome], {
+        overwrite: true
+    });
+
+    return app.toTree(merged);
 };
