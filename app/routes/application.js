16bc4f5be9d04461401cf4029f5ee273e6056299
Elia <frodohack@gmail.com>
2017-08-18 15:44:45 +0200

    Added support to command key for shortcuts

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,11 +2,10 @@ import Ember from 'ember';
 import utils from '../utils';
 
 export default Ember.Route.extend({
-
     setupController(...args) {
         this._super(...args);
 
-        utils.addShortcut('ctrl+o', () => {
+        utils.addShortcut(['command+o', 'ctrl+o'], () => {
             this.controllerFor('application').openFile();
         });
     },
