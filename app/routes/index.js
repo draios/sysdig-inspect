d55cf64ec55f0c2e95c61ab2f9aeb22c200a4b68
Elia <frodohack@gmail.com>
2017-08-18 10:15:14 +0200

    Open file from welcome screen

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    beforeModel() {
-        // this.replaceWith('capture', 'lo.scap');
-    },
 
     setupController(...args) {
         this._super(...args);
 
         this.controllerFor('application').set('captureFilePath', null);
+    },
+
+    actions: {
+        openFile() {
+            this.controllerFor('application').openFile();
+        }
     },
 });
