e73f75522013207853ab93399967cff05fb15537
davideschiera <davide.schiera@gmail.com>
2017-08-19 23:34:00 -0700

    Table and view data

--- app/routes/capture/views/view.js
+++ app/routes/capture/views/view.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        return {
+            id: params.id,
+            filePath: this.modelFor('capture').filePath,
+        };
+    },
 });
