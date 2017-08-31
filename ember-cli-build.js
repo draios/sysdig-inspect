137a3508f6ad7993342dd8c3e70f34a913031e77
davideschiera <davide.schiera@gmail.com>
2017-08-31 10:46:02 -0700

    Move Mousetrap to ui-toolkit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,7 +26,6 @@ module.exports = function(defaults) {
     // along with the exports of each module as its value.
 
     app.import(app.bowerDirectory + '/normalize-css/normalize.css');
-    app.import(app.bowerDirectory + '/mousetrap/mousetrap.min.js');
 
     return app.toTree();
 };
