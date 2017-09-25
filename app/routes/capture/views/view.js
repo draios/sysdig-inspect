091065436d68692072742852071055ff5a4db07d
davideschiera <davide.schiera@gmail.com>
2017-09-25 01:32:01 -0700

    Track views

--- app/routes/capture/views/view.js
+++ app/routes/capture/views/view.js
@@ -23,7 +23,9 @@ export default Ember.Route.extend({
     },
 
     viewsManager: Ember.inject.service('views-manager'),
+    drilldownManager: Ember.inject.service('drilldown-manager'),
     dataSearchService: Ember.inject.service('data-search'),
+    userTracking: Ember.inject.service('user-tracking'),
 
     model(params) {
         return new ViewModel(params.id, params.filter, params.searchPattern, this.modelFor('capture'));
@@ -45,6 +47,42 @@ export default Ember.Route.extend({
                 document.title = `Sysdig Inspect - ${view.name} on ${model.capture.filePath}`;
             })
         ;
+
+        // NOTE: Time selection can be changed with the mouse, and this would perform a visit every
+        // "tick"
+        Ember.run.debounce(this, this.trackVisit, 300);
+    },
+
+    trackVisit() {
+        this.get('userTracking').visit({
+            route: 'capture.views.view',
+            view: this.get('controller.model.viewId'),
+            'sysdig filter': this.get('controller.model.filter'),
+            find: this.get('controller.model.searchPattern'),
+            from: this.get('controller.model.capture.queryParams.timeFrom'),
+            to: this.get('controller.model.capture.queryParams.timeTo'),
+            timelines: serializeTimelines(this.get('controller.model.capture.queryParams.metricTimelinesParam')),
+            'drill down': serializeDrillDown(this.get('drilldownManager'), this.get('controller.model.viewId'), this.get('controller.model.capture.queryParams.drilldownInfoParam')),
+        });
+
+        function serializeTimelines(param) {
+            return (param || '').split(',').join(', ');
+        }
+
+        function serializeDrillDown(drilldownManager, viewId, param) {
+            if (param) {
+                const steps = drilldownManager.convertFromUrl({
+                    viewId,
+                    drilldownInfoParam: param,
+                });
+
+                return steps.map((step) => {
+                    return `${step.viewId} > ${step.selection || 'no selection'}`;
+                }).join(', ');
+            } else {
+                return '';
+            }
+        }
     },
 
     deactivate() {
