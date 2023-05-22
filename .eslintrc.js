feaff8b99c47715730c9766b846fb323d7c14b08
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-05-22 16:11:17 +0000

    update: bumped ember to 3.8
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,14 +20,12 @@ module.exports = {
     // node files
     {
       files: [
-        '.eslintrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
+        'lib/*/index.js'
       ],
       parserOptions: {
         sourceType: 'script',
