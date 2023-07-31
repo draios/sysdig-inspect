3f4ef0557f3ee456f64403144ec7fd714501b125
Francesco Napoletano <francesco.napoletano@sysdig.com>
2023-07-31 18:42:17 +0200

    [Internal] testing embedded binaries

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SYSDIG_VERSION="0.27.1"
-SYSDIG_VERSION_MAC="0.27.0"
+SYSDIG_VERSION="0.32.0"
+SYSDIG_VERSION_MAC="0.32.0"
 
 # Env parameters
 # - CLEANUP (default: true)
@@ -117,9 +117,8 @@ install_dependencies() {
             # Mac binaries
             
             mkdir -p deps/sysdig-mac
-            
-            curl https://download.sysdig.com/dependencies/sysdig-${SYSDIG_VERSION_MAC}-mac.zip -o sysdig.zip
-            unzip -d deps/sysdig-mac sysdig.zip
+            # this is an hack
+            unzip -d deps/sysdig-mac sysdig-0.32.0-mac.zip
         fi
     fi
 }
